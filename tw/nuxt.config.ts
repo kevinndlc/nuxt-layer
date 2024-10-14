f0359e9304eda9ca8b4d24e511a5f76067854360
Kevin Ndlc <34909931+kevinndlc@users.noreply.github.com>
2024-10-14 12:51:38 +0200

    Update nuxt.config.ts

--- tw/nuxt.config.ts
+++ tw/nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['../base'],
+  extends: [['github:kevinndlc/nuxt-layer/base', {install: true}]],
   modules: ['@nuxtjs/tailwindcss'],
 });
