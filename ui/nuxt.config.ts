14b5b441946ebedbafd79e9d0dd7b3b0bf4815e1
Kevin Ndlc <kevin@nedellec.eu>
2024-10-14 13:22:14 +0200

    feat: add nuxt ui layer

---
+++ ui/nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: [['github:kevinndlc/nuxt-layer/base', {install: true}]],
+  modules: ['@nuxt/ui'],
+});
